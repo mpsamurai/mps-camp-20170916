164f59aca9c5452a70ed217d19b40b4f74d6f239
hiroshi teraoka <hiroshi4000@gmail.com>
2017-09-16 22:28:35 +0900

    次元数の調整

--- teraoka/neuron/neuron_sample.py
+++ teraoka/neuron/neuron_sample.py
@@ -1,23 +1,21 @@
-import numpy as np
-
-
 class Neuron:
-    def __init__(self):
-        self.w = np.random.rand(2)  # [ , ] 2次元
-        self.b = np.random.rand(1)  # [] 1次元
+    def __init__(self, n):
+        self.w = np.random.rand(n)  # setされた次元数
+        self.b = np.random.rand(1)  # [] 1次元の要素をバイアスに与える
 
-    def neuron(self, x):
+    def set_state(self, x):
         s0 = (x.dot(self.w)) + 1 * self.b
         return s0
 
-    def output(self, s0):
+    def activate(self, s0):
         y = 1 / (1 + np.exp(-s0))
         return y
 
+    def output(self, x):
+        return self.activate(self.set_state(x))
+
 
 if __name__ == '__main__':
-    n = Neuron()
-    x = np.array([1., 2.])
-    i = n.neuron(x)
-    print(n.output(i))
-
+    n = Neuron(3)  # 3次元に設定
+    x = np.array([1., 2., 0.])
+    print(n.output(x))
